c4ee2dc56cc31a1d5162d34380721b12b7282a04
hao <760442816@qq.com>
2023-05-03 19:06:37 +0800

    修正eslint

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -1,8 +1,8 @@
 <script setup lang='ts'>
-import { defineAsyncComponent, ref } from 'vue'
+import { ref } from 'vue'
 import { HoverButton, SvgIcon, UserAvatar } from '@/components/common'
 
-const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
+// const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
 
 const show = ref(false)
 </script>
